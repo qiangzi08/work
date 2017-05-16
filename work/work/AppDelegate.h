994c2b422842ab01c0a71b0eaa346da88093eb78
qiangzi08 <edz@edzdeMacBook-Pro.local>
2017-05-16 09:20:37 +0800

    first commit

---
+++ work/work/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  work
+//
+//  Created by edz on 2017/5/16.
+//  Copyright © 2017年 杜永强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ work/work/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  work
+//
+//  Created by edz on 2017/5/16.
+//  Copyright © 2017年 杜永强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
